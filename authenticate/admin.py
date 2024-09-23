7983d7c3890a27e9612553f3a3c96e22d47bde19
LIMO2001 <98254234+LIMO2001@users.noreply.github.com>
2024-09-23 16:22:45 +0300

    Admin page

--- authenticate/admin.py
+++ authenticate/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from booking.models import *
 
 
 class MoversAdmin(UserAdmin):
@@ -8,5 +9,6 @@ class MoversAdmin(UserAdmin):
     list_display = ['username', 'email', 'first_name', 'last_name', 'is_active']
     list_filter = ['is_active']  # Ensure this is a list or tuple
     search_fields = ['username', 'email', 'first_name', 'last_name', 'is_active']
-
-admin.site.register(CustomUser, MoversAdmin)
+admin.site.register(ServiceBooking)
+admin.site.register(Driver)
+admin.site.register(Vehicle)
